dc0d6ea0f2d13cec5367406cf4528ce385e9a00f
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-03-23 14:51:10 +0000

    added error messages for maximum and minimum, and conditional for power saving mode accordingly

--- jasmine/jasmine-standalone-3.1.0 (1)/spec/thermostat_spec.js
+++ jasmine/jasmine-standalone-3.1.0 (1)/spec/thermostat_spec.js
@@ -5,31 +5,88 @@ describe('Thermostat', function() {
     powerSavingMode = true
   });
 
-  describe('Thermostat', function() {
+  describe('Basic functions', function() {
 
     it('starts at 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20)
     });
 
-    it('increases temperature', function() {
-      expect(thermostat.up()).toEqual(21)
+    it('has a minimum temperature', function() {
+      expect(thermostat.minimum).toEqual(10)
     });
+  });
 
+  describe('#down', function() {
     it('decreases temperature', function() {
       expect(thermostat.down()).toEqual(19)
     });
 
-    it('has a minimum temperature', function() {
-      expect(thermostat.minimum()).toEqual(10)
+    it('raises an error when you hit minimum', function() {
+      thermostat.temperature = 10
+      expect(function(){thermostat.down();}).toThrowError('You reached the minimum');
+    });
+  });
+
+  describe('#up', function() {
+    it('increases temperature', function() {
+      expect(thermostat.up()).toEqual(21)
     });
 
-    // it('has a maximum temperature', function() {
-    //   expect(thermostat.maximum()).toEqual(25)
-    // });
-    //
-    // it('changes a maximum temperature', function() {
-    //   powerSavingMode = false
-    //   expect(thermostat.maximum()).toEqual(32)
-    // });
+    it('raises an error when you hit maximum', function() {
+      thermostat.temperature = 25
+      thermostat.powerSavingModeOn
+      expect(function(){thermostat.up();}).toThrowError('You reached the maximum');
+    });
+
+    it('raises an error when you hit maximum with power saving mode on', function() {
+      thermostat.temperature = 32
+      thermostat.powerSavingModeOff
+      expect(function(){thermostat.up();}).toThrowError('You reached the maximum');
+    });
+  });
+
+  describe('#powerSavingModeOn', function() {
+    it('sets a maximum temperature', function() {
+      expect(thermostat.maximum).toEqual(25)
+    });
+
+    it('returns true for powerSavingMode', function() {
+      expect(thermostat.powerSavingMode).toBe(true)
+    });
+  });
+
+  describe('#powerSavingModeOff', function() {
+    it('changes a maximum temperature', function() {
+      thermostat.powerSavingModeOff();
+      expect(thermostat.maximum).toEqual(32)
+    });
+
+    it('returns false for powerSavingMode', function() {
+      thermostat.powerSavingModeOff();
+      expect(thermostat.powerSavingMode).toBe(false)
+    });
+  });
+
+  describe('#reset', function() {
+    it('brings temperature back to default', function() {
+      expect(thermostat.temperature).toEqual(20)
+    });
+  });
+
+  describe('#currentEnergyUsage', function() {
+    it('returns "low usage" when < 18', function() {
+      thermostat.temperature = 16
+      expect(thermostat.currentEnergyUsage()).toEqual('low-usage')
+    });
+
+    it('returns "medium usage" when < 25', function() {
+      thermostat.temperature = 23
+      expect(thermostat.currentEnergyUsage()).toEqual('medium-usage')
+    });
+
+    it('returns "high usage" when > 25', function() {
+      thermostat.temperature = 28
+      expect(thermostat.currentEnergyUsage()).toEqual('high-usage')
+    });
   });
 });
--- jasmine/jasmine-standalone-3.1.0 (1)/src/thermostat.js
+++ jasmine/jasmine-standalone-3.1.0 (1)/src/thermostat.js
@@ -1,25 +1,55 @@
-function Thermostat(temperature = 20, minimum = 10, maximum = 25, powerSavingMode = true) {
+function Thermostat(temperature = 20, minimum = 10, maximum = 25, maximum_pws = 32, powerSavingMode = true) {
   this.temperature = temperature
-  this._DEFAULT_MINIMUM = minimum
+  this.minimum = minimum
   this.maximum = maximum
+  this.maximum_pws = maximum_pws
   this.powerSavingMode = powerSavingMode
 }
 
 Thermostat.prototype.up = function() {
-  return this.temperature += 1
+  if (this.powerSavingMode = true) {
+    if (this.temperature + 1 > this.maximum) {
+      throw new Error('You reached the maximum');
+    } else {
+      return this.temperature += 1;
+    }
+  } else {
+    if (this.temperature + 1 > this.maximum_pws) {
+      throw new Error('You reached the maximum');
+    } else {
+      return this.temperature += 1;
+    }
+  }
 };
 
 Thermostat.prototype.down = function() {
-  return this.temperature -= 1
+  if (this.temperature - 1 < this.minimum) {
+    throw new Error('You reached the minimum');
+  } else {
+    return this.temperature -= 1;
+  }
 };
 
-Thermostat.prototype.minimum = function() {
-  return this._DEFAULT_MINIMUM
+Thermostat.prototype.powerSavingModeOn = function() {
+  this.maximum = maximum
+  this.powerSavingMode = true
 };
 
-// Thermostat.prototype.maximum = function() {
-//   if powerSavingMode = true
-//     return this._DEFAULT_MAXIMUM
-//   else
-//     return this._DEFAULT_MAXIMUM + 7
-// };
+Thermostat.prototype.powerSavingModeOff = function() {
+  this.maximum = this.maximum_pws
+  this.powerSavingMode = false
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.currentEnergyUsage = function() {
+  if (this.temperature < 18) {
+    return 'low-usage';
+  } else if (this.temperature > 25) {
+    return 'high-usage';
+  } else {
+    return 'medium-usage';
+  }
+};
